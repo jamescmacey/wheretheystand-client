f7bcfae08b09bbade7c2f7ee92744a21844d34a8
James Macey <james@jamescmacey.com>
2025-10-29 21:44:53 +1300

    Removed reCaptcha, again
    Maybe it will build successfully on Cloudflare this time :)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/algolia', '@pinia/nuxt', 'nuxt-gtag', 'vue-recaptcha/nuxt', 'nuxt-mapbox', '@nuxt/ui', '@nuxtjs/robots'],
+  modules: ['@nuxtjs/algolia', '@pinia/nuxt', 'nuxt-gtag', 'nuxt-mapbox', '@nuxt/ui', '@nuxtjs/robots'],
   css: [
     '~/assets/css/main.css',
   ],
